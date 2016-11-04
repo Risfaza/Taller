93c53aa6af11cda87b8996c8933494e0dee40c99
Memo <g_gomez@outlook.com>
2016-11-04 10:21:52 -0600

    Agrega la Descripcion en las tablas de Percepciones y Deducciones

--- Cacao/war/esquemascontroller.js
+++ Cacao/war/esquemascontroller.js
@@ -34,7 +34,7 @@ app.controller("EsquemasController",function($scope,ToDoService){
 	$scope.listdeducciones=[
 	{
 		"clave": "001",
-		"descripcion": "Deducccion1 fdsf fdf fds",
+		"descripcion": "Deducccion1",
 	},
 	{
 		"clave": "002",
@@ -50,18 +50,28 @@ app.controller("EsquemasController",function($scope,ToDoService){
 	$scope.newEsq = {};
 	$scope.newDed ={};
 	$scope.listaEsq=[];
-	$scope.addPer=function(){
+	$scope.addPer=function(listpercecpciones){
 		//ToDoService.add($scope.newEsq);
-		var esq=$scope.inputPerc;
+		var esq=$scope.inputPerc;		
+		for(var i=0; i<$scope.listpercecpciones.length;i++){
+			if($scope.listpercecpciones[i].clave==esq.clave){
+				esq.descripcion=$scope.listpercecpciones[i].descripcion
+			}
+		}
 		$scope.regimen.percepciones.push(esq);
 		$scope.inputPerc ={};		
 	}
-	$scope.addDed=function(){
+	$scope.addDed=function(listdeducciones){
+
 		var ded=$scope.inputDeduc;
+		for(var i=0; i<$scope.listdeducciones.length;i++){
+			if($scope.listdeducciones[i].clave==ded.clave){
+				ded.descripcion=$scope.listdeducciones[i].descripcion
+			}
+		}
 		$scope.regimen.deducciones.push(ded);
 		$scope.inputDeduc ={};	
 	}
-
 	$scope.removeEmp = function(item){
 		//$scope.todo = ToDoService.removeItem(item);
 		$scope.listaEsq.removeItem(item);		
