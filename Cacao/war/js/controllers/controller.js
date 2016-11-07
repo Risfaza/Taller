8e93e60622bb4819ca454404cb7cca5bc5b6770f
guill <guill@LAPTOP-8BBOI0AH>
2016-11-07 11:06:09 -0600

    Agrega el Alta usuario html y quitar etiquetas body

--- Cacao/war/js/controllers/controller.js
+++ Cacao/war/js/controllers/controller.js
@@ -2,23 +2,33 @@ var app = angular.module("app", ['ngRoute']);
 app.config(['$routeProvider',function($routeProvider) {
 
 
-  $routeProvider.when('/empresas', {
-    templateUrl: "pages/empresas.html",
-    controller: "empresasController"
+ $routeProvider.when('/empresas', {
+  templateUrl: "pages/empresas.html",
+  controller: "empresasController"
+});
+
+ $routeProvider.when('/esquemas', {
+  templateUrl: "pages/esquemas.html",
+  controller: "esquemasController"
+});
+
+ $routeProvider.when('/empleados', {
+  templateUrl: "pages/empleados.html",
+  controller: "empleadosController"
+});
+
+ $routeProvider.when('/altausuarios', {
+  templateUrl: "pages/altausuarios.html",
+  /*controller: "usuariosController"*/
   });
 
-  $routeProvider.when('/esquemas', {
-    templateUrl: "pages/esquemas.html",
-    controller: "esquemasController"
-  });
+ $routeProvider.when('/empresas/list', {
+  templateUrl: "pages/empresasList.html",
+  controller: "empresasListController"
+});
 
-  $routeProvider.when('/empleados', {
-    templateUrl: "pages/empleados.html",
-    controller: "empleadosController"
-  });
-
-  $routeProvider.otherwise({
-    redirectTo: 'index.html'
-  });   
+ $routeProvider.otherwise({
+  redirectTo: 'index.html'
+});   
 
 }]);
