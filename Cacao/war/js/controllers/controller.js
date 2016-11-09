02ac78bf6b7144ca533e7d323c5498572f5f3db8
memo <memo@LAPTOP-8BBOI0AH>
2016-11-09 10:31:50 -0600

    Generacion de PDF (CFDI)

--- Cacao/war/js/controllers/controller.js
+++ Cacao/war/js/controllers/controller.js
@@ -25,16 +25,21 @@ app.config(['$routeProvider',function($routeProvider) {
   templateUrl: "pages/empresasList.html",
   controller: "empresasListController"
 });
+ $routeProvider.when('/cfdi', {
+  templateUrl: "pages/cfdi.html",
+  controller: "cfdiController"
+});
 
  $routeProvider.when('/empresas/edit/:rfc', {
-	  templateUrl: "pages/empresas.html",
-	  controller: "empresasEditController"
-	});
+   templateUrl: "pages/empresas.html",
+   controller: "empresasEditController"
+ });
  
  $routeProvider.when('/empresas/details/:rfc', {
-	  templateUrl: "pages/empresasDetails.html",
-	  controller: "empresasDetailsController"
-	});
+   templateUrl: "pages/empresasDetails.html",
+   controller: "empresasDetailsController"
+ });
+
  $routeProvider.otherwise({
   redirectTo: 'index.html'
 });   
