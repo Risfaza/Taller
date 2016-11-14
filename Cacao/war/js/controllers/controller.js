5c0b91191c6347da82ba470ebbe48a21f36cddf2
memogomez <guillermogomezmaya@gmail.com>
2016-11-14 16:48:01 -0600

    Cambio en el css de tablas

--- Cacao/war/js/controllers/controller.js
+++ Cacao/war/js/controllers/controller.js
@@ -1,43 +1,63 @@
 var app = angular.module("app", ['ngRoute']);
 app.config(['$routeProvider',function($routeProvider) {
 
- $routeProvider.when('/empresas', {
-  templateUrl: "pages/empresas.html",
-  controller: "empresasController"
-});
+	$routeProvider.when('/empresas', {
+		templateUrl: "pages/empresas.html",
+		controller: "empresasController"
+	});
+	
+	$routeProvider.when('/esquemas/agregar/:rfc', {
+		templateUrl: "pages/esquemas.html",
+		controller: "esquemasController"
+	});
 
- $routeProvider.when('/esquemas', {
-  templateUrl: "pages/esquemas.html",
-  controller: "esquemasController"
-});
+	$routeProvider.when('/empleados', {
+		templateUrl: "pages/empleados.html",
+		controller: "empleadosController"
+	});
 
- $routeProvider.when('/empleados', {
-  templateUrl: "pages/empleados.html",
-  controller: "empleadosController"
-});
+	$routeProvider.when('/altausuarios', {
+		templateUrl: "pages/altausuarios.html",
+		/*controller: "usuariosController"*/
+	});
 
- $routeProvider.when('/altausuarios', {
-  templateUrl: "pages/altausuarios.html",
-  /*controller: "usuariosController"*/
-});
+	$routeProvider.when('/empresas/list', {
+		templateUrl: "pages/empresasList.html",
+		controller: "empresasListController"
+	});
 
- $routeProvider.when('/empresas/list', {
-  templateUrl: "pages/empresasList.html",
-  controller: "empresasListController"
-});
+	$routeProvider.when('/cfdi', {
+		templateUrl: "pages/cfdi.html",
+		controller: "cfdiController"
+	});
 
-$routeProvider.when('/cfdi', {
-  templateUrl: "pages/cfdi.html",
-  controller: "cfdiController"
-});
+	$routeProvider.when('/cfdisinqr', {
+		templateUrl: "pages/cfdisinqr.html",
+		controller: "cfdiController"
+	});
 
-$routeProvider.when('/cfdisinqr', {
-  templateUrl: "pages/cfdisinqr.html",
-  controller: "cfdiController"
-});
+	$routeProvider.when('/empresas/edit/:rfc', {
+		templateUrl: "pages/empresas.html",
+		controller: "empresasEditController"
+	});
+	
+	$routeProvider.when('/empresas/details/:rfc', {
+		templateUrl: "pages/empresasDetails.html",
+		controller: "empresasDetailsController"
+	});
+	
+	$routeProvider.when('/esquemas/details/:id', {
+		templateUrl: "pages/esquemasDetails.html",
+		controller: "esquemasDetailsController"
+	});
+	
+	$routeProvider.when('/esquemas/edit/:id', {
+		templateUrl: "pages/esquemasEdit.html",
+		controller: "esquemasEditController"
+	});
 
-$routeProvider.otherwise({
-  redirectTo: 'index.html'
-});   
+	$routeProvider.otherwise({
+		redirectTo: 'index.html'
+	});   
 
 }]);
