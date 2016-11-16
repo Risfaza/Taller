fedf1e5a84f75837bb8496e907f101119df4fa90
Israel <Israel@LAPTOP-M9C5PA8Q>
2016-11-16 16:30:17 -0600

    Lista de empleados

--- Cacao/war/js/controllers/empleadoscontroller.js
+++ Cacao/war/js/controllers/empleadoscontroller.js
@@ -5,28 +5,42 @@ app.service('empleadosService', function() {
 	};
 	this.clean = function() {/* Limpia o Elimina todos los elementos */
 	};
-	this.getAll = function() {/* Muestra todos los Elementos */
+	this.getAll = function(rfc) {/* Muestra todos los Elementos */
 	};
 	this.removeItem = function(item) {/* Elimina elemento por elemeto */
 	}
 })
-app.controller("empleadosController", [ '$scope', '$http', 'empleadosService',function($scope, $http, empleadosService) {
-	$scope.addEmp = function() {
-		$http.post("/employee/add", $scope.newEmp).then(function(response) {
-			alert("Empleado Guardado");
-			console.log(response.data);
-		}, function(response) {
-			alert("Something went wrong!");
-			console.log(response);
-		});
-	}
-} 
-]);
+app.controller("empleadosController", [
+		'$scope',
+		'$http',
+		'empleadosService',
+		function($scope, $http, empleadosService) {
+			$scope.addEmp = function() {
+				$http.post("/employee/add", $scope.newEmp).then(
+						function(response) {
+							alert("Empleado Guardado");
+							console.log(response.data);
+						}, function(response) {
+							alert("Something went wrong!");
+							console.log(response);
+						});
+			}
+		} ]);
 app.controller("empleadosDetailsController", [ '$scope', '$http', '$location',
 		'$routeParams', 'empleadosService',
 		function($scope, $http, $location, $routeParams, empresasService) {
-			
-			$scope.regresaEmpresa=function(){
-				$location.path("/empresas/details/"+$routeParams.rfc);
+
+			$scope.regresaEmpresa = function() {
+				$location.path("/empresas/details/" + $routeParams.rfc);
 			}
-} ]);
+		} ]);
+
+app.controller("empleadosListController", [ '$scope', '$http', '$location',
+		'$routeParams', 'empleadosService',
+		function($scope, $http, $location, $routeParams, empresasService) {
+
+			
+			$scope.regresaEmpresa = function() {
+				$location.path("/empresas/details/" + $routeParams.rfc);
+			}
+		} ]);
